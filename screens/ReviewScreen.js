9efd53297dfe921ae91b4f476bc05e18c96dc063
Luke Przekwas <galactica@The-Galactica.local>
2018-06-20 12:03:48 -0500

    styled settings button

--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Platform } from 'react-native';
 import { Button } from 'react-native-elements';
 
 class ReviewScreen extends Component {
@@ -8,8 +8,16 @@ class ReviewScreen extends Component {
         return {
             title: 'Review Jobs',
             headerRight: (
-                <Button title='Settings' onPress={() => navigation.navigate('settings')} />
-            )
+                <Button
+                    title='Settings'
+                    backgroundColor='rgba(0,0,0,0)'
+                    color='rgba(0, 122, 255, 1)'
+                    onPress={() => navigation.navigate('settings')}
+                />
+            ),
+            style: {
+                marginTop: Platform.OS === 'android' ? 24 : 0 
+            }
         }
     };
 
